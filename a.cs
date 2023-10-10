3db752358aec0c851625dc1fd92f235312def25a
Darley Leal <darleyleal98@gmail.com>
2023-10-10 05:34:57 -0300

    New updates

---
+++ a.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Program 
+{
+    public class Program 
+    {
+        static void Main() 
+        {
+            System.Console.WriteLine("Hello");
+        }
+    }
+}
